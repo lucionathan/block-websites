dd4f4ecf2c9f65fdb87861955ccd07baa3ed8856
lucionathan <42965703+lucionathan@users.noreply.github.com>
2023-04-25 09:12:11 -0300

    Update block_websites.sh
    removing typo

--- block_websites.sh
+++ block_websites.sh
@@ -7,7 +7,6 @@ fi
 
 arr=('linkedin.com' 'twitch.tv' 'instagram.com' 'facebook.com' 'twitter.com' 'twttr.com' 'twitter.fr' 'twitter.jp' 'twitter.rs' 'twitter.uz' 'twitter.biz' 'twitter.dk' 'twitter.events' 'twitter.ie' 'twitter.je' 'twitter.mobi' 'twitter.nu' 'twitter.pro' 'twitter.su' 'twitter.vn' 'twitter.com' 'twitter.gd' 'twitter.im' 'twitter.hk' 'twitter.jp' 'twitter.ch' 'twitter.pt' 'twitter.rs' 'twitter.com.br' 'twitter.ae' 'twitter.eus' 'ns1.p34.dynect.net' 'ns2.p34.dynect.net' 'ns3.p34.dynect.net' 'ns4.p34.dynect.net' 'd01-01.ns.twtrdns.net' 'd01-02.ns.twtrdns.net' 'a.r06.twtrdns.net' 'b.r06.twtrdns.net' 'c.r06.twtrdns.net' 'api-34-0-0.twitter.com' 'api-47-0-0.twitter.com' 'cheddar.twitter.com' 'goldenglobes.twitter.com' 'mx003.twitter.com' 'pop-api.twitter.com' 'spring-chicken-an.twitter.com' 'spruce-goose-ae.twitter.com' 'takeflight.twitter.com' 'www2.twitter.com')
 
-
 function update_script() {
     local action=$1
     local website=$2
